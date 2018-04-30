2f48e8633049e4bf883ed873c7d15161299cef80
Randy Reitz <rsreitz@outlook.com>
2018-04-30 14:24:26 -0400

    Storing numerics as numerics in the Friends scores.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -32,31 +32,14 @@ module.exports = function(app) {
         // Using a RegEx Pattern to remove spaces from newfriend
         // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
         newFriend.userName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-        console.log(newFriend);
-
+        // console.log(newFriend.scores)
+        //convert our scores to numerics...
+        newFriend.scores = newFriend.scores.map(function(x) {
+            return parseInt(x, 10);
+        });
+        // console.log(newFriend.scores)
         friends.push(newFriend);
 
         res.json(friends);
     });
 }
-
-
-
-
-
-
-// // Create New friends - takes in JSON input
-// app.post("/api/friends", function(req, res) {
-//     // req.body hosts is equal to the JSON post sent from the user
-//     // This works because of our body-parser middleware
-//     var newfriend = req.body;
-
-
-
-//     console.log(newfriend);
-
-//     friends.push(newfriend);
-
-//     res.json(newfriend);
-// });
