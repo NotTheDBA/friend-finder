5b23108c0219aa9b15ac91ebedcb3c6968fc432d
Randy Reitz <rsreitz@outlook.com>
2018-05-02 11:10:49 -0400

    Added friend matching logic.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 var friends = [{
-    "name": "Randy",
+    "name": "Mild Lee",
     "photo": "/images/DSC_0165_cr.jpg",
     "scores": [
         3,
@@ -13,7 +13,39 @@ var friends = [{
         3,
         3
     ],
-    "userName": "randy"
+    "userName": "mildlee"
+}, {
+    "name": "Extreme Lee",
+    "photo": "/images/DSC_0165_cr.jpg",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ],
+    "userName": "extremelee"
+}, {
+    "name": "Bland Lee",
+    "photo": "/images/DSC_0165_cr.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ],
+    "userName": "blandlee"
 }]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,15 +14,9 @@ module.exports = function(app) {
     // Displays a single friend, or returns false
     app.get("/api/friends/:friend", function(req, res) {
         var chosen = req.params.friend;
-        var stats = req.params.stats;
-
-        console.log(chosen);
-        console.log(stats);
 
         for (var i = 0; i < friendList.length; i++) {
             if (chosen === friendList[i].userName) {
-                // return res.json(friendList[i]);
-                // return res.redirect("/view?friend=" + chosen);
                 return res.json(friendList[i]);
 
             }
@@ -45,12 +39,36 @@ module.exports = function(app) {
             return parseInt(x, 10);
         });
 
+        var bestFriends = [];
+        var matchScore = 999;
+        friendList.forEach(friend => {
+            var newmatch = 0
+            for (i = 0; i < friend.scores.length; i++) {
+                newmatch += Math.abs(friend.scores[i] - newFriend.scores[i]);
+            }
+            console.log("matchscore: " + newmatch);
+            // friend.scores.every(function(u, i) {
+            //     newmatch += Math.abs(u - newFriend.scores[i]);
+            //     // console.log("matchscore: " + newmatch);
+            // })
+            if (newmatch < matchScore) {
+                bestFriends = [];
+                bestFriends.push(friend);
+                matchScore = newmatch;
+            } else if (newmatch === matchScore) {
+                bestFriends.push(friend);
+            }
+        });
+        // console.log("matchscore: " + matchScore);
+
+        console.log(bestFriends[0]);
+
         // console.log(newFriend.scores)
         friendList.push(newFriend);
 
         // This doesn't play nice with the jQuery...
         // return res.redirect("/");
-        res.end(res.json(newFriend.userName));
+        res.end(res.json(bestFriends[0].userName));
 
     });
 }
